9ac819e4505524cc32a64dcf35851bf57d9ed0ac
peter leeh <leeh123peter@gmail.com>
2024-04-24 11:15:30 +0200

    compose wrapper

--- src/compose.rs
+++ src/compose.rs
@@ -1,4 +1,7 @@
-use std::process::Command;
+use std::{
+    io::Write,
+    process::{Command, Stdio},
+};
 
 #[derive(Debug)]
 pub struct Compose {
@@ -7,6 +10,8 @@ pub struct Compose {
     // pub stderr: std::io::Stderr,
     // pub cmd: std::process::Command,
 }
+
+const CONTENT: &'static str = include_str!("../docker-compose.yml");
 
 #[derive(Default)]
 pub struct ComposeOpts;
@@ -18,16 +23,53 @@ impl ComposeOpts {
 }
 
 impl Compose {
-    pub fn up(files: &[&str], opts: Option<ComposeOpts>) {
-        let args: Vec<&str> = files.iter().flat_map(|file| ["-f", file]).collect();
+    pub fn up_custom(files: &[&str], opts: Option<ComposeOpts>) -> anyhow::Result<()> {
+        let files = files
+            .iter()
+            .flat_map(|file| ["-f", file])
+            .collect::<Vec<_>>();
         let opts = opts.unwrap_or_default().into_args();
-        let cmd = Command::new("docker")
+        let mut child = Command::new("docker")
+            .stdout(Stdio::null())
             .arg("compose")
-            .args(args)
+            .args(files)
             .arg("up")
             .args(opts)
-            .output();
+            .spawn()
+            .unwrap();
 
-        println!("{:#?}", cmd);
+        let status = child.wait().expect("Failed to wait for docker-compose");
+        if !status.success() {
+            eprintln!("docker-compose failed with exit code: {}", status);
+        }
+
+        Ok(())
+    }
+    pub fn up_builtin(opts: Option<ComposeOpts>) -> anyhow::Result<()> {
+        let opts = opts.unwrap_or_default().into_args();
+        let mut child = Command::new("docker")
+            .stdin(Stdio::piped())
+            .stdout(Stdio::null()) // TODO
+            .arg("compose")
+            .arg("-f")
+            .arg("-")
+            .arg("up")
+            .args(opts)
+            .spawn()
+            .unwrap();
+
+        let Some(mut stdin) = child.stdin.take() else {
+            anyhow::bail!("Failed to get stdin for docker-compose")
+        };
+        stdin
+            .write_all(CONTENT.as_bytes())
+            .expect("Failed to write to stdin");
+        drop(stdin);
+        let status = child.wait().expect("Failed to wait for docker-compose");
+        if !status.success() {
+            eprintln!("docker-compose failed with exit code: {}", status);
+        }
+
+        Ok(())
     }
 }
